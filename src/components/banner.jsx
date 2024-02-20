732285595f0d250f3a81a68ca92f8f4f2f4a65da
Muh. Afdal Maulana Said <afdalmaulanaaa@gmail.com>
2024-02-20 21:25:28 +0700

    update icon in figma, and px

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,16 +1,16 @@
 import { FaArrowRightLong } from "react-icons/fa6";
 export default function Banner() {
   return (
-    <div className="w-full px-[6rem] py-[10rem] ">
-      <div className="text-blue-700 text-7xl font-semibold">
+    <div className="bg-cover w-full px-[6rem] py-[10rem] ">
+      <div className="text-[#2269D8] text-[80px] font-semibold">
         Buy Local Sell Global
       </div>
-      <div className="ml-12 mt-10 text-4xl">
+      <div className="ml-12 text-[40px]">
         Suistainable Products for the world
       </div>
       <div className="flex">
         <div className="relative">
-          <button className="text-blue-700 border rounded-xl w-[18rem] border-blue-700 px-10 py-4 mt-10">
+          <button className="text-[#2269D8] border rounded-xl w-[18rem] border-blue-700 px-10 py-4 mt-10">
             Explore Marketplace
           </button>
           <FaArrowRightLong className="absolute text-blue-700 text-2xl top-[3.5rem] right-[2rem]" />
--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,31 +1,47 @@
-import { FaTshirt } from "react-icons/fa";
-import { SiHomeassistant } from "react-icons/si";
-import { GrYoga } from "react-icons/gr";
-import { GiOpenBook } from "react-icons/gi";
-import { FaHiking } from "react-icons/fa";
+import fashion from "../../public/Fashoin.svg";
+import homeGarden from "../../public/HomeGarden.svg";
+import healthBeauty from "../../public/HealthandBeauty.svg";
+import officeSchool from "../../public/OfficeandSchool.svg";
+import activiesOutdoors from "../../public/Activities.svg";
+import Image from "next/image";
 export default function Menu() {
   return (
     <div className="border-b-4 w-full flex justify-between px-[7rem] h-[10rem] ">
-      <div className="hover:bg-green-300 w-[12rem] px-[4rem] py-[2.5rem]">
-        <FaTshirt className="text-6xl text-blue-700" />
-        <div>Fashion</div>
-      </div>
-      <div className="hover:bg-green-300 w-[11.6rem] px-[2rem] py-[2.5rem]">
-        <SiHomeassistant className="text-6xl text-blue-700 ml-7" />
-        <div>Home & Garden</div>
-      </div>
-      <div className="hover:bg-green-300 w-[11.6rem] px-[2rem] py-[2.5rem]">
-        <GrYoga className="text-6xl text-blue-700 ml-7" />
-        <div>Health & Beauty</div>
-      </div>
-      <div className="hover:bg-green-300 w-[11.6rem] px-[2rem] py-[2.5rem]">
-        <GiOpenBook className="text-6xl text-blue-700 ml-7" />
-        <div>Office & School</div>
-      </div>
-      <div className="hover:bg-green-300 w-[12rem] px-[1rem] py-[2.5rem]">
-        <FaHiking className="text-6xl text-blue-700 ml-12" />
-        <div>Activities & Outdoors</div>
-      </div>
+      <Image
+        src={fashion}
+        alt="fashion"
+        width={400}
+        height={400}
+        className="hover:bg-[#BBFD01]"
+      />
+      <Image
+        src={homeGarden}
+        alt="Home Garden"
+        width={400}
+        height={400}
+        className="hover:bg-[#BBFD01]"
+      />
+      <Image
+        src={healthBeauty}
+        alt="Home Garden"
+        width={400}
+        height={400}
+        className="hover:bg-[#BBFD01]"
+      />
+      <Image
+        src={officeSchool}
+        alt="Home Garden"
+        width={400}
+        height={400}
+        className="hover:bg-[#BBFD01]"
+      />
+      <Image
+        src={activiesOutdoors}
+        alt="Home Garden"
+        width={400}
+        height={400}
+        className="hover:bg-[#BBFD01]"
+      />
     </div>
   );
 }
